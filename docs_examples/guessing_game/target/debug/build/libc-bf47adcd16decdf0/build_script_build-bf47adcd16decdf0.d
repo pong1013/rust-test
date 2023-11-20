a9cbab0fe5db08aaf30878e943d1116e882becaf
pong1013 <pong861013@gmail.com>
2023-11-20 13:17:06 +0000

    Add example: guessing_game

---
+++ docs_examples/guessing_game/target/debug/build/libc-bf47adcd16decdf0/build_script_build-bf47adcd16decdf0.d
@@ -0,0 +1,5 @@
+/workspaces/rust/rust-test/guessing_game/target/debug/build/libc-bf47adcd16decdf0/build_script_build-bf47adcd16decdf0: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.149/build.rs
+
+/workspaces/rust/rust-test/guessing_game/target/debug/build/libc-bf47adcd16decdf0/build_script_build-bf47adcd16decdf0.d: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.149/build.rs
+
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.149/build.rs:
---
+++ docs_examples/guessing_game/target/debug/deps/rand_core-e8f203a4101860c0.d
@@ -0,0 +1,12 @@
+/workspaces/rust/rust-test/guessing_game/target/debug/deps/rand_core-e8f203a4101860c0.rmeta: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/block.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/error.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/impls.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/le.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/os.rs
+
+/workspaces/rust/rust-test/guessing_game/target/debug/deps/librand_core-e8f203a4101860c0.rlib: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/block.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/error.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/impls.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/le.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/os.rs
+
+/workspaces/rust/rust-test/guessing_game/target/debug/deps/rand_core-e8f203a4101860c0.d: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/block.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/error.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/impls.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/le.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/os.rs
+
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/lib.rs:
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/block.rs:
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/error.rs:
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/impls.rs:
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/le.rs:
+/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_core-0.6.4/src/os.rs:
