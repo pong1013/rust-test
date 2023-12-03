09b9fff41fe75fff846255ed6630a598e1ae5350
pong1013 <pong861013@gmail.com>
2023-12-03 03:40:50 +0000

    Add examples: automated_test

---
+++ docs_examples/automated_test/examples/test_fn.rs
@@ -0,0 +1,18 @@
+pub fn greeting(name: &str) -> String {
+    String::from("哈囉！")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("卡爾");
+        assert!(
+            result.contains("卡爾"),
+            "打招呼時並沒有喊出名稱，其數值為 `{}`",
+            result
+        );
+    }
+}
---
+++ docs_examples/automated_test/target/debug/libautomated_test.d
@@ -0,0 +1,1 @@
+/workspaces/rust/rust-test/docs_examples/automated_test/target/debug/libautomated_test.rlib: /workspaces/rust/rust-test/docs_examples/automated_test/src/lib.rs
