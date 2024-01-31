5b9cdf99477256606ab45c6baac59261dd3ae2dd
pong1013 <pong861013@gmail.com>
2024-01-31 11:16:05 +0000

    Add examples: Guessing game

--- docs_examples/guessing_game/target/debug/deps/rand_chacha-2b4125b800404f31.d
+++
@@ -1,7 +0,0 @@
-/workspaces/rust/rust-test/guessing_game/target/debug/deps/rand_chacha-2b4125b800404f31.rmeta: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/workspaces/rust/rust-test/guessing_game/target/debug/deps/rand_chacha-2b4125b800404f31.d: /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs:
-/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs:
-/usr/local/cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs:
--- docs_examples/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/workspaces/rust/rust-test/guessing_game/target/debug/guessing_game: /workspaces/rust/rust-test/guessing_game/src/main.rs
